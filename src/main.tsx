ba107bed2064a1210f07107e2477ab2be3353507
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-05-15 22:14:35 +0100

    Changed to hasrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import FigmaPortfolio from "./Pages/FigmaPortfolio.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import FigmaWorks from "./Pages/FigmaWorks.tsx";
 import FigmaWorkDetails from "./Pages/FigmaWorkDetails.tsx";
 import FigmaBlog from "./Pages/FigmaBlog.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<FigmaPortfolio />} />
         <Route path="/works" element={<FigmaWorks />} />
         <Route path="/works/details" element={<FigmaWorkDetails />} />
         <Route path="/blog" element={<FigmaBlog />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
