9b66ebd16920bbf87475eabaa7f3d6096a350051
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-05-15 22:32:14 +0100

    Alternate Router

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import FigmaPortfolio from "./Pages/FigmaPortfolio.tsx";
-import { HashRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import FigmaWorks from "./Pages/FigmaWorks.tsx";
 import FigmaWorkDetails from "./Pages/FigmaWorkDetails.tsx";
 import FigmaBlog from "./Pages/FigmaBlog.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/FigmaPortfolio">
       <Routes>
         <Route path="/" element={<FigmaPortfolio />} />
         <Route path="/works" element={<FigmaWorks />} />
         <Route path="/works/details" element={<FigmaWorkDetails />} />
         <Route path="/blog" element={<FigmaBlog />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
